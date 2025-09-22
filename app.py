5bf9f22931a6bdacb71fe9ac78af0213547bcf4f
Ben <Benporreca04@gmail.com>
2025-09-22 17:10:13 -0400

    landing page data can be sent over

--- app.py
+++ app.py
@@ -1,41 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 import sqlalchemy as sa
 
 engine = sa.create_engine("mysql+mysqldb://root:B3njamin178@localhost:3306/sakila", echo=True)
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
-
-
-@app.route("/hello/<name>")
-def hello_there(name):
-    return "Hello there " + name
-
-@app.route("/films")
-def films():
-    with engine.connect() as conn:
-        result = conn.execute(sa.text("SELECT * FROM film"))
-        for row in result:
-            print(row)
-    return "Printed all films to the console!"
-
-@app.route("/script1")
-def script1():
-    with engine.connect() as conn:
-        result = conn.execute(sa.text("""
-            select 	film.film_id, 
-                    film.title, 
-                    category.name 
-            from film 
-            join film_category on film.film_id = film_category.film_id
-            join category on film_category.category_id = category.category_id;
-        """))
-        for row in result:
-            print(row)
-    return "Display film id, title, and film category name"
 
 @app.route("/top5films")
 def script2():
@@ -54,6 +25,41 @@ def script2():
             order by rental_count desc
             limit 5;
         """))
-        for row in result:
-            print(row)
-    return "TOP 5 FILMS"
+        data = [dict(row._mapping) for row in result]
+    return jsonify(data)
+
+@app.route("/top5actors")
+def script3():
+    with engine.connect() as conn:
+        result = conn.execute(sa.text("""
+            select 	a.actor_id,
+                    a.first_name,
+                    a.last_name,
+                    count(fa.film_id) as film_count
+            from actor a 
+            join film_actor fa on a.actor_id = fa.actor_id 
+            group by a.actor_id, a.first_name, a.last_name 
+            order by film_count desc
+            limit 5;
+        """))
+        data = [dict(row._mapping) for row in result]
+    return jsonify(data)
+
+@app.route("/actorInfo/<actor>")
+def script4(actor):
+    with engine.connect() as conn:
+        result = conn.execute(sa.text("""
+            select f.film_id,
+                    f.title,
+                    count(r.rental_id) as rental_count
+            from rental r
+            join inventory i on r.inventory_id  = i.inventory_id 
+            join film f on i.film_id = f.film_id
+            join film_actor fa on f.film_id = fa.film_id 
+            where fa.actor_id = :actor_id
+            group by f.film_id, f.title 
+            order by rental_count desc
+            limit 5;
+        """), {"actor_id": actor})
+        data = [dict(row._mapping) for row in result]
+    return jsonify(data)
